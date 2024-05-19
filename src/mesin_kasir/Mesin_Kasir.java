cfb475a3e91a627966c35e201d88c6336a26d071
Jidan39 <a.fauzan090305@gmail.com>
2024-05-19 22:48:28 +0700

    First

---
+++ src/mesin_kasir/Mesin_Kasir.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mesin_kasir;
+
+/**
+ *
+ * @author USER
+ */
+public class Mesin_Kasir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        koneksi k = new koneksi();
+        k.connect();
+    }
+    
+}
